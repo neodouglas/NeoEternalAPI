fe468d4a1d9b227d8552067233754fcf59f9e613
Douglas Soares Silva <44717994+neodouglas@users.noreply.github.com>
2022-08-28 07:23:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from flask import *
 import os
 import aminofix
+import heroku3
 os.system('set FLASK_ENV=development')
 app = Flask(__name__)
 
+def restart(key, name):
+    heroku_conn = heroku3.from_key(key)
+    botapp = heroku_conn.apps()[name]
+    botapp.restart()
 
 @app.route('/api/login',  methods = ['POST'])
 def get_timezone():
-  data = request.form
-  login = data.get("login")
-  password = data.get("password")
-  device = data.get("device")
-  client = aminofix.Client(device)
-  client.login(email = login, password = password)
-  return f"{client.sid}"
+    try:
+        data = request.form
+        login = data.get("login")
+        password = data.get("password")
+        device = data.get("device")
+        key = data.get("key")
+        name = data.get("name")
+        client = aminofix.Client(device)
+        client.login(email = login, password = password)
+        return f"{client.sid}"
+    except:
+        try:
+            restart(key, name)
+            return abort(404)
+        except:
+            return abort(404)
+
+
 
 if __name__ == '__main__':
   app.run("0.0.0.0", 5000)
